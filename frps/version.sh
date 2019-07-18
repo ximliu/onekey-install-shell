94118a459e2efeba79b36376daa3b009cd45d19d
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2019-07-18 21:46:15 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -2,6 +2,6 @@
 frpver=$(wget -qO- https://github.com/fatedier/frp/releases/latest | grep "<title>" |sed -r 's/.*Release (.+) · fatedier.*/\1/')
 #echo ${frpver:1} 
 export FRPS_VER=${frpver:1}
-export FRPS_INIT="https://raw.githubusercontent.com/wxlost/onekey-install-shell/master/frps/frps.init"
+export FRPS_INIT="https://raw.githubusercontent.com/ximliu/onekey-install-shell/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
